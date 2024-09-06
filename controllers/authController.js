43504acd109aa77866d61facc0e8a1602f3aead0
freetimezp <kraftukrnet@ukr.net>
2024-09-06 19:00:48 +0300

    07. js-portfolio-122-blood-bank. Login API. check credential.

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,9 @@
 const userModel = require("../models/userModel");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
+
+//register
 const registerController = async (req, res) => {
     try {
         const existingUser = await userModel.findOne({ email: req.body.email });
@@ -37,4 +40,47 @@ const registerController = async (req, res) => {
     }
 };
 
-module.exports = { registerController };
+//login
+const loginController = async (req, res) => {
+    try {
+        //try find user in database
+        const user = await userModel.findOne({ email: req.body.email });
+
+        //if user not found in database
+        if (!user) {
+            return res.status(404).send({
+                success: false,
+                message: "user not found in db.."
+            });
+        }
+
+        //compare password
+        const comparePassword = await bcrypt.compare(req.body.password, user.password);
+        if (!comparePassword) {
+            return res.status(500).send({
+                success: false,
+                message: "invalid credential"
+            });
+        }
+
+        //create token
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1d' });
+
+        //result 
+        return res.status(200).send({
+            success: true,
+            message: "Login success",
+            token,
+            user
+        })
+    } catch (error) {
+        console.log("login controller error: ", error.message);
+        res.status(500).send({
+            success: false,
+            message: "invalid credential",
+            error
+        });
+    }
+};
+
+module.exports = { registerController, loginController };
